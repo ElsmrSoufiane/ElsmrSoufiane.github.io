d281696260223a586098799c49053366433fc35d
ElsmrSoufiane <soufianeasmar0@gmail.com>
2025-02-14 12:33:51 +0100

    add products

--- src/data.js
+++ src/data.js
@@ -1,50 +1,89 @@
 const produit = [
-    {
-      id: 0,
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnNI-KcDnsu46f9OrmcixXg9_tPEbPfOdl9Q&s",
-      nom: "Huile d'argan pure",
-      prix: "250Dhs",
-      description: "Huile d'argan 100% biologique, idéale pour la peau et les cheveux.",
-      histoire: "Extraite des arganiers du Souss, cette huile précieuse est utilisée depuis des siècles pour ses vertus hydratantes et réparatrices."
-    },
-    {
-      id: 1,
-      image: "https://achagros.com/wp-content/uploads/2021/03/savon-noir-marocain.jpg",
-      nom: "Savon noir marocain",
-      prix: "80Dhs",
-      description: "Savon noir naturel à base d'huile d'olive, parfait pour le gommage.",
-      histoire: "Utilisé dans les hammams traditionnels marocains, il prépare la peau à une exfoliation en profondeur."
-    },
-    {
-      id: 2,
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQliXFhEwvky4xsA-9c8yKZqxzBGrYiotxEjQ&s",
-      nom: "Tapis berbère",
-      prix: "1200Dhs",
-      description: "Magnifique tapis tissé à la main par les artisans berbères.",
-      histoire: "Chaque tapis est unique et raconte une histoire à travers ses motifs et ses couleurs."
-    },
-    {
-      id: 3,
-      image: "https://media.istockphoto.com/id/1163551942/fr/photo/d%C3%A9tail-de-la-th%C3%A9i%C3%A8re-marocaine-traditionnelle-avec-le-th%C3%A9-%C3%A0-la-menthe-et-le-sucre.jpg?s=1024x1024&w=is&k=20&c=zYmgkECO5e0jROeTKmU0VJcbvfggunMEk-AYCql-ZFA=",
-      nom: "Théière marocaine en argent",
-      prix: "450Dhs",
-      description: "Théière traditionnelle en métal finement ciselé, idéale pour le thé à la menthe.",
-      histoire: "La cérémonie du thé est un rituel sacré au Maroc, symbole d'hospitalité et de convivialité."
-    },
-    {
-      id: 4,
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh7uVbaK91h3qRtcA4wUY_8Lw0CeILyBe5fg&s",
-      nom: "Babouches en cuir",
-      prix: "200Dhs",
-      description: "Chaussures traditionnelles marocaines en cuir véritable, faites à la main.",
-      histoire: "Les babouches sont portées depuis des siècles par les Marocains, alliant confort et élégance."
-    },
-    {
-      id: 5,
-      image: "https://img.leboncoin.fr/api/v1/lbcpb1/images/a9/1c/36/a91c36071a78c4e6c5b01f9ed90bbe9a52073ca2.jpg?rule=ad-large",
-      nom: "Lanterne artisanale en métal",
-      prix: "350Dhs",
-      description: "Lanterne en métal finement travaillée, apportant une ambiance chaleureuse.",
-      histoire: "Inspirées de l'architecture andalouse, ces lanternes illuminent les riads marocains avec élégance."
-    }];
+  {
+    id: 0,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2Iy0HPEiz_kfFg10s7k09rSN_LsVVW2mIyw&s",
+    nom: "Huile d'argan pure",
+    prix: "250Dhs",
+    description: "Huile d'argan 100% biologique, idéale pour la peau et les cheveux.",
+    histoire: "Extraite des arganiers du Souss, cette huile précieuse est utilisée depuis des siècles pour ses vertus hydratantes et réparatrices."
+  },
+  {
+    id: 1,
+    image: "https://www.lespetitesexperiences.com/wp-content/uploads/2023/04/6d79f4e21a9411e2eb8d82087d65fdde.jpg",
+    nom: "Savon noir marocain",
+    prix: "80Dhs",
+    description: "Savon noir naturel à base d'huile d'olive, parfait pour le gommage.",
+    histoire: "Utilisé dans les hammams traditionnels marocains, il prépare la peau à une exfoliation en profondeur."
+  },
+  {
+    id: 2,
+    image: "https://cooparttissagetam.com/wp-content/uploads/2023/11/2.jpg",
+    nom: "Tapis berbère",
+    prix: "1200Dhs",
+    description: "Magnifique tapis tissé à la main par les artisans berbères.",
+    histoire: "Chaque tapis est unique et raconte une histoire à travers ses motifs et ses couleurs."
+  },
+  {
+    id: 3,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwlVgqaW3bYAXjmCSCV39b0ABF__UG1sCnZg&s",
+    nom: "Théière marocaine en argent",
+    prix: "450Dhs",
+    description: "Théière traditionnelle en métal finement ciselé, idéale pour le thé à la menthe.",
+    histoire: "La cérémonie du thé est un rituel sacré au Maroc, symbole d'hospitalité et de convivialité."
+  },
+  {
+    id: 4,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz3nrm1tJzr4n3tsdjmQCKnkmWu2ZtUDpDzA&s",
+    nom: "Babouches en cuir",
+    prix: "200Dhs",
+    description: "Chaussures traditionnelles marocaines en cuir véritable, faites à la main.",
+    histoire: "Les babouches sont portées depuis des siècles par les Marocains, alliant confort et élégance."
+  },
+  {
+    id: 5,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVgGR1O_U9w9gIwjt_W0xi-r72qmSga4llFw&s",
+    nom: "Lanterne artisanale en métal",
+    prix: "350Dhs",
+    description: "Lanterne en métal finement travaillée, apportant une ambiance chaleureuse.",
+    histoire: "Inspirées de l'architecture andalouse, ces lanternes illuminent les riads marocains avec élégance."
+  },
+  {
+    id: 6,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxag9V-U8FyCwXyJBj6sCatWCx0kC4Y4Ge6Q&s",
+    nom: "Table en Zellige Artisanal",
+    prix: "1800Dhs",
+    description: "Une table carrée ou ronde ornée de mosaïques zellige traditionnelles, fabriquée à la main par des artisans de Fès.",
+    histoire: "Le zellige est une forme d'art marocaine datant du Xe siècle, originaire de Fès."
+  },
+  {
+    id: 7,
+    image: "https://www.mavaisselle-store.com/3404-large_default/tajine-en-terre-cuite-35cm.jpg",
+    nom: "Tajine Traditionnel en Terre Cuite",
+    prix: "300Dhs",
+    description: "Un tajine artisanal en terre cuite, parfait pour cuisiner des plats marocains traditionnels.",
+    histoire: "Le tajine est un ustensile de cuisine utilisé depuis des siècles au Maroc pour une cuisson lente et uniforme."
+  },
+  {
+    id: 8,
+    image: "https://img.cuisineaz.com/660x660/2016/06/21/i68333-the-a-la-menthe.jpeg",
+    nom: "Thé Vert à la Menthe",
+    prix: "80Dhs",
+    description: "Un mélange de thé vert Gunpowder et de menthe fraîche, offrant une saveur rafraîchissante et authentique.",
+    histoire: "Introduit au Maroc au XIXe siècle, le thé à la menthe est devenu un symbole de l'hospitalité marocaine."
+  },
+  {
+    id: 9,
+    image: "https://epicerie-bio.herbio.ma/wp-content/uploads/2024/02/sel_rose_de_l-himalaya-1.jpg",
+    nom: "Sel Rose de l'Atlas",
+    prix: "120Dhs",
+    description: "Un sel naturel extrait des montagnes de l'Atlas, riche en minéraux et parfait pour assaisonner les plats.",
+    histoire: "Le sel rose de l'Atlas est récolté depuis des siècles dans les mines marocaines."
+  },
+
+];
+
+
+
+
+
 export  {produit};
